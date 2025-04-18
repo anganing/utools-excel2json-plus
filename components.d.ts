8842663e7296794a6af97cf9428517d0c4b90c47
tangsc <1419054707@qq.com>
2025-04-18 11:42:54 +0800

    fix:基础bug修复，可运行可打包

--- components.d.ts
+++ components.d.ts
@@ -4,7 +4,6 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     NButton: typeof import('naive-ui')['NButton']
     NConfigProvider: typeof import('naive-ui')['NConfigProvider']
     NDivider: typeof import('naive-ui')['NDivider']
